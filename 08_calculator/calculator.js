c4bc9c2b4ff7e68628f1afafb0e47704ebf1c4bf
Hi_Joshi <35184227+himanshuhjoshi@users.noreply.github.com>
2024-09-18 06:55:51 +0530

    resuming the exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(max,min) {
+  if(min>max) [max, min] = [min, max];
+  return max-min;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+	array.forEach((element) => result += element);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  array.forEach(element => result *= element);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(arg) {
+  if(arg == 0 || arg == 1) {return 1}
+  else{
+    let result = 1;
+    for(let i=1; i<=arg; i++){
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
